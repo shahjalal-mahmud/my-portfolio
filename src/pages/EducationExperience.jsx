3c0b0a85a4dbe5979af2c3f2ad65d52288fe0563
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-05-19 20:42:09 +0600

    refactor: remove Roadmap component and related data from the project

--- src/pages/EducationExperience.jsx
+++ src/pages/EducationExperience.jsx
@@ -1,14 +1,12 @@
 // src/pages/EducationExperience.jsx
 import EducationTimeline from "../components/EducationTimeline";
 import Experience from "../components/Experience";
-import Roadmap from "../components/Roadmap";
 
 const EducationExperience = () => {
   return (
     <>
       <EducationTimeline />
       <Experience />
-      {/* <Roadmap /> */}
     </>
   );
 };
