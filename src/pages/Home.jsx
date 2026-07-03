37704fd3f4a779ab120bd70dc669e456df54e694
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-07-04 02:11:52 +0600

    refactor(home): drop per-page ToastContainer now mounted globally in main.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,12 @@
 // src/pages/Home.jsx
 import Hero from "../sections/Hero";
 import About from "../sections/About";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <About />
-      <ToastContainer position="top-right" autoClose={3000} />
     </>
   );
 };
